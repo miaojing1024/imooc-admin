6d4b79a66e4cebabd893cb57d4cfcebe646243f5
miaojing <a1024621284@163.com>
2022-03-03 15:32:44 +0800

    本地master分支

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@ export default {
     HelloWorld
   },
   created() {
-    console.log('我是master分支')
+    console.log('我是本地master分支')
     // // const arr = ['编号1', '编号2', '编号3', '编号4', '编号5']
     // // for (const i in arr) {
     // //   console.log(i) // 0 1 2 3 4
