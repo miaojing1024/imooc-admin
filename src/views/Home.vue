939506444ba737efc8e738626927478619b7483d
miaojing <a1024621284@163.com>
2022-03-14 16:44:51 +0800

    feat: 使用lodash实现节流

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Vue logo" src="../assets/logo.png" @click="imgOnclick"/>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -8,40 +8,25 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { throttle } from 'lodash-es'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
   },
+  data() {
+    return {}
+  },
   created() {
-    console.log('我是本地dev-test分支')
-    console.log('本次提交测试代码回滚')
-    // const arr = ['编号1', '编号2', '编号3', '编号4', '编号5']
-    // for (const i in arr) {
-    //   console.log(i) // 0 1 2 3 4
-    //   // key打印的是索引值 0 1 2 3 4
-    //   // arr[key]打印的为数据  编号1 编号2 编号3 编号4 编号5
-    // }
-    // for (const i of arr) {
-    //   console.log(i) // 编号1 编号2 编号3 编号4 编号5
-    // }
-    var obj = {
-      name: 'fqniu',
-      age: 25,
-      sex: '男'
+  },
+  methods: {
+    imgOnclick: throttle(function() {
+      this.funLog()
+    }, 1000, { leading: false, trailing: true }),
+    funLog() {
+      console.log(111111)
     }
-    for (var key in obj) {
-      console.log(key)
-      // key打印的是属性 name age sex
-      // arr[key]打印的为属性值 fqniu 25 男
-    }
-    /*
-      for in可以遍历对象，for of不能遍历对象
-      for in不能遍历map集合，for of可以用来遍历map集合
-      for in遍历数组得到的是数组的下标，for of遍历数组得到的是数组的元素
-      for in遍历键  for of遍历值
-    */
   }
 }
 </script>
