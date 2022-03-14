ca4a69c521bd2f046318524e319193c2e3a0c67a
miaojing <a1024621284@163.com>
2022-03-14 18:11:55 +0800

    feat: 使用事件委托实现点击事件

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" @click="imgOnclick"/>
+    <div>
+      <ul @click="ulClick">
+        <li v-for="(item,index) in 5" :key="index">
+          <div data-type="file">附件下载</div>
+          <div data-type="img">图片</div>
+          <div>啊啊啊啊啊</div>
+        </li>
+      </ul>
+    </div>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -26,6 +35,15 @@ export default {
     }, 1000, { leading: false, trailing: true }),
     funLog() {
       console.log(111111)
+    },
+    ulClick(event) {
+      const currentEle = event.target
+      console.log(currentEle.dataset.type)
+      if (currentEle.dataset.type === 'file') {
+        console.log('下载附件')
+      } else if (currentEle.dataset.type === 'img') {
+        console.log('下载图片')
+      }
     }
   }
 }
